de2fb58e009f8a711e5f4aee457bbdcef3a93d15
Andre Carlos da Silva <andrecarlos@gmail.com>
2019-05-01 11:32:15 -0400

    modified the package of tests

--- reactive-examples/src/test/java/acs/springframework/reactiveexamples/ReactiveExamplesApplicationTests.java
+++ reactive-examples/src/test/java/acs/springframework/reactiveexamples/ReactiveExamplesApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.reactiveexamples;
+package acs.springframework.reactiveexamples;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- reactive-examples/src/test/java/acs/springframework/reactiveexamples/ReactiveExamplesTest.java
+++ reactive-examples/src/test/java/acs/springframework/reactiveexamples/ReactiveExamplesTest.java
@@ -1,4 +1,4 @@
-package guru.springframework.reactiveexamples;
+package acs.springframework.reactiveexamples;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
